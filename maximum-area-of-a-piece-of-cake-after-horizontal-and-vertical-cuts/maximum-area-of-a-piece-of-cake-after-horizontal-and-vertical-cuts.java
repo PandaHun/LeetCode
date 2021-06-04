93348e323b4b6cfed446ef540e8b630add336649
Dahun Jeong <gnsgnsek@gmail.com>
2021-06-04 14:35:02 +0900

    Time: 25 ms (5.30%), Space: 59.1 MB (5.38%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,21 +1,17 @@
 class Solution {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        int beforeH = 0;
-        int beforeV = 0;
-        int maxH = 0;
-        int maxV = 0;
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
-        for (int now : horizontalCuts) {
-            maxH = Math.max(maxH, now - beforeH);
-            beforeH = now;
-        }
-        for (int now : verticalCuts) {
-            maxV = Math.max(maxV, now - beforeV);
-            beforeV = now;
-        }
-        maxH = Math.max(maxH, h - beforeH);
-        maxV = Math.max(maxV, w - beforeV);
+        int maxH = getMax(horizontalCuts, h);
+        int maxV = getMax(verticalCuts, w);
         return (int) ((long) maxH * maxV % 1000000007);
+    }
+
+    private int getMax(int[] arr, int len) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            max = Math.max(max, arr[i] - arr[i - 1]);
+        }
+        return Math.max(max, len - arr[arr.length - 1]);
     }
 }
